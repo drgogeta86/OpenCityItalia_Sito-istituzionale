0df285acf0378ea91eb12b9961bb6d45ce0fb2f8
Luca Realdi <lr@opencontent.it>
2021-11-06 23:34:34 +0100

    Add some blacklisted file extensions

--- conf.d/ez/override/file.ini.append.php
+++ conf.d/ez/override/file.ini.append.php
@@ -16,5 +16,11 @@ DBConnectRetries=3
 DBExecuteRetries=20
 MaxCopyRetries=5
 
+[FileSettings]
+FileExtensionBlackList[]=exe
+FileExtensionBlackList[]=sh
+FileExtensionBlackList[]=bash
+FileExtensionBlackList[]=java
+FileExtensionBlackList[]=bat
 
 */ ?>
